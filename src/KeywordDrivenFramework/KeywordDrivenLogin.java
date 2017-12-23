bf1cfe5d6a11b37d58d34e33507348272f3ced71
japho <ja4oti@gmail.com>
2017-12-23 20:06:11 +0300

    keyword driven finished

--- src/KeywordDrivenFramework/KeywordDrivenLogin.java
+++ src/KeywordDrivenFramework/KeywordDrivenLogin.java
@@ -1,10 +1,56 @@
 package KeywordDrivenFramework;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.*;
+import config.ActionKeywords;
+import utility.ExcelUtils;
 
 public class KeywordDrivenLogin {
 
-	
+	public static void main(String[] args) throws Exception {
+		// Declaring the path of the Excel File with the name of the Excel File
+		String sPath = "C:\\Users\\Tezza\\eclipse-workspace\\ECommerceStore\\src\\ExcelFiles\\KeywordDrivenFramework.xlsx";
+
+		// passing the Excel path and SheetName as arguments to connect with Excel File
+		ExcelUtils.setExcelFile(sPath, "Test Steps");
+
+		// This is the loop for reading the values of the column 3(Action Keywords) row
+		// by row
+
+		for (int iRow = 1; iRow <= 10; iRow++) {
+			// storing value of excel cell in sActionKeyword string variable
+			String sActionKeyword = ExcelUtils.getCellData(iRow, 2);
+
+			// comparing value of Excel Cell with all the project keywords
+			if (sActionKeyword.equals("openBrowser")) {
+				ActionKeywords.openBrowser();
+			} else if (sActionKeyword.equals("navigate")) {
+				ActionKeywords.navigate();
+			}
+
+			else if (sActionKeyword.equals("openLoginpage")) {
+				ActionKeywords.openLoginpage();
+			}
+
+			else if (sActionKeyword.equals("inputEmail")) {
+				ActionKeywords.inputEmail();
+			}
+
+			else if (sActionKeyword.equals("inputPassword")) {
+				ActionKeywords.inputPassword();
+			}
+
+			else if (sActionKeyword.equals("clickLoginbutton")) {
+				ActionKeywords.clickLoginbutton();
+			}
+
+			else if (sActionKeyword.equals("Logout")) {
+				ActionKeywords.Logout();
+			}
+
+			else if (sActionKeyword.equals("closeBrowser")) {
+				ActionKeywords.closeBrowser();
+			}
+
+		}
+	}
+
+}
